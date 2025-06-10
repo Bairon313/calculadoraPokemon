5fa82cbcc4eeddf48d7dbb7f005070f3080474d5
Bairon313 <baironvalencia588@gmail.com>
2025-06-09 20:00:10 -0400

    Delete calculadora_resta.py

--- calculadora_resta.py
+++
@@ -1,9 +0,0 @@
-num1=int(input("ingrese valor 1: "))
-num2=int(input("ingrese valor 2: "))
-
-print("la suma es ",num1+num2)
-
-num1=int(input("ingrese valor 1: "))
-num2=int(input("ingrese valor 2: "))
-
-print("la resta es ",num1-num2)
